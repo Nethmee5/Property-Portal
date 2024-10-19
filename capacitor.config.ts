2f9b3cf49a326524ede0ae7f7f6dd2806ffee58a
Nethmee5 <kumararatnenethmee@gmail.com>
2024-10-19 11:01:52 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'property-portal',
+  webDir: 'dist'
+};
+
+export default config;
